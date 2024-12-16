46f736bbf99a216625e425a5c912200d2c25d376
AYYAPPA RAJU G <AYYAPPA RAJU G@DESKTOP-LG5P40C>
2024-12-16 12:45:24 +0530

    Add project files.

---
+++ Shopping/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index1(int? id, string name, double? price)
+        {
+            ViewData["Id"] = id;
+            ViewData["Name"] = name;
+            ViewData["Price"] = price;
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [ActionName("c")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View("Contact");
+        }
+        public ActionResult rk()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Shopping/Controllers/RkController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping.Controllers
+{
+    public class RkController : Controller
+    {
+        // GET: Rk
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
